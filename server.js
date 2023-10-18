8b4b6da61629c3a3f4df25592f4c27ac16d6561c
dada216 <63715342+dada216@users.noreply.github.com>
2023-10-18 17:45:28 +0200

    Update server.js

--- server.js
+++ server.js
@@ -6,5 +6,5 @@ app.use(express.static("express"));// default URL for website
 app.use('/', function(req,res){
     res.sendFile(path.join(__dirname+'/express/index.html'));
   });const server = http.createServer(app);
-const port = 3000;
+const port = 8080;
 server.listen(port);console.debug('Server listening on port ' + port);
